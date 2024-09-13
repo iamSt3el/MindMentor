1f49600af75d67a9cca9fb12caeebc1aa5600b03
iamSt3el <himanshudhaka9166@gmail.com>
2024-09-13 19:05:30 +0530

    update 1

---
+++ src/pages/Homepage/index.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import styles from "./homepage.module.scss";
+
+function Homepage() {
+  return (
+    <div className={styles.outerBox}>
+    </div>
+  );
+}
+
+export default Homepage;
